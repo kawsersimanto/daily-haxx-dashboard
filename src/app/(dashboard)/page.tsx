1b0fcb914a7194c694e2e777558539a0c23d7ee3
Kawser Simanto <kawsersimanto@gmail.com>
2025-09-25 20:45:43 +0600

    create dashboard header

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,8 +1,12 @@
 import { Breadcrumbs } from "@/components/breadcrumbs/Breadcrumbs";
+import { Dashboard } from "@/features/dashboard/components/Dashboard";
 
 const DashboardPage = () => {
   return (
     <>
+      <div className="bg-white py-6 md:px-10 px-5 shadow-[0_5px_20px_0_rgba(38,3,71,0.06)]">
+        <h2>Dashboard</h2>
+      </div>
       <div>
         <Breadcrumbs
           manual={[
@@ -10,8 +14,8 @@ const DashboardPage = () => {
             { label: "Home", href: "/" },
           ]}
         />
+        <Dashboard />
       </div>
-      asd
     </>
   );
 };
---
+++ src/features/dashboard/components/Dashboard.tsx
@@ -0,0 +1,3 @@
+export const Dashboard = () => {
+  return <section className="p-10">Dashboard</section>;
+};
