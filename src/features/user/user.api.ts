832002f25974f0e748d8917e67333be387771b28
Kawser Simanto <kawsersimanto@gmail.com>
2025-10-09 14:46:39 +0600

    fixed build errors

--- src/features/user/user.api.ts
+++ src/features/user/user.api.ts
@@ -1,20 +1,20 @@
+import { IUser } from "@/features/user/user.interface";
 import { baseApi } from "@/redux/api/baseApi";
-import { User } from "./user.interface";
 
 export const userApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getUsers: builder.query<User[], void>({
+    getUsers: builder.query<IUser[], void>({
       query: () => "/user",
     }),
-    getUserById: builder.query<User, string>({
+    getUserById: builder.query<IUser, string>({
       query: (id) => `/user/${id}`,
     }),
-    createUser: builder.mutation<User, Partial<User>>({
+    createUser: builder.mutation<IUser, Partial<IUser>>({
       query: (body) => ({ url: "/user", method: "POST", body }),
     }),
-    updateUser: builder.mutation<User, Partial<User> & { id: string }>(
-      { query: ({ id, ...body }) => ({ url: `/user/${id}`, method: "PUT", body }) }
-    ),
+    updateUser: builder.mutation<IUser, Partial<IUser> & { id: string }>({
+      query: ({ id, ...body }) => ({ url: `/user/${id}`, method: "PUT", body }),
+    }),
     deleteUser: builder.mutation<{ success: boolean; id: string }, string>({
       query: (id) => ({ url: `/user/${id}`, method: "DELETE" }),
     }),
